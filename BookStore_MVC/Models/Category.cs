f52b8b1d8ff38b302e8fbba3b6b1c10a4cf4a4dc
nishant-nez <nishant.khadka@deerwalk.edu.np>
2025-02-07 23:26:48 +0545

    Configure database, add category controller and create view

---
+++ BookStore_MVC/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_MVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }  // if name is Id or <name>Id then it will be considered as primary key automatically
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
--- BookStore_MVC/Program.cs
+++ BookStore_MVC/Program.cs
@@ -1,7 +1,12 @@
+using BookStore_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -17,7 +22,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+ 
 app.UseAuthorization();
 
 app.MapControllerRoute(
